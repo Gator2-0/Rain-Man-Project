92839f3807e27a80db6ee2c994e8db8c77cb595e
Jeremy <jejedech.63@gmail.com>
2023-08-08 21:18:04 +1000

    adding details to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,6 +7,13 @@ const Home = () => {
     <div className="home-container">
       <Container fluid className="text-light py-5">
         <h1>This is the Home Page</h1>
+        <h2> Always wanted to be an expert at Black Jack, You want to look smart?
+            <br></br>
+            Just try any of the options above:<br></br>
+              -Simple game let's you play a BlackJack game, help you make the right decision<br></br>
+              -User-input game let's you tell us what card have been dealt in your game<br></br>
+              -AR... You get it, like Iron-Man supe rsunnies!!!
+        </h2>
       </Container>
     </div>
   )
