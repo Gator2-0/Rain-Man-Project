5d72cf6e8cc61210db16b54f81b40cefe67415f2
Jeremy <jejedech.63@gmail.com>
2023-08-26 09:40:05 +1000

    adding suit styling to card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  BsFillSuitHeartFill,
+  BsFillSuitDiamondFill,
+  BsFillSuitSpadeFill,
+  BsFillSuitClubFill,
+} from 'react-icons/bs';
+import { GiCardJoker } from 'react-icons/gi';
 import './Card.css';
 
-// const Card = ({ rank, suit, value }) => {
-//   return(
-//     <div className="card">
-//       <div className="card-rank">{rank}</div>
-//       <div className={`card-suit ${suit}`}>{suit}</div>
-//       <div className="card-value">{value}</div>
-//     </div>
-//   )
+ 
 
-
-// }
+function SuitIcon({ suit, style }) {
+  if (suit === "hearts") {
+    return <BsFillSuitHeartFill style={style} />;
+  } else if (suit === "diamonds") {
+    return <BsFillSuitDiamondFill style={style} />;
+  } else if (suit === "spades") {
+    return <BsFillSuitSpadeFill style={style} />;
+  } else if (suit === "clubs") {
+    return <BsFillSuitClubFill style={style} />;
+  } else {
+    // If no suit is given then we can just put out little joker faces
+    return <GiCardJoker style={style} />;
+  }
+}
 
 const Card = ({ rank, suit, value }) => {
+
+  const [suitColor, setSuitColor] = useState(""); // Define suitColor state 
+  useEffect(() => {
+    suit === "Hearts" || suit === "Diamonds"
+      ? setSuitColor("red")
+      : setSuitColor("black");
+  }, [suit]); 
+
   return(
     <div className="card">
       <h1 className='rank top'>
         {rank}
       </h1>
+      <SuitIcon
+        suit={suit}
+        style={{
+          fontSize: "25px",
+          position: "absolute",
+          margin: "0px",
+          top: "75px",
+          left: "100px",
+          color: suitColor,
+        }}
+      />
       <h1 className='rank bottom'>
         {rank}
       </h1>
     </div>
   )
-
-
 }
 
 
