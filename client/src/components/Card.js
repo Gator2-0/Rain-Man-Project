7d1e7780357e3c7995c68e6bf228c551f0b04c77
Jeremy <jejedech.63@gmail.com>
2023-08-26 09:03:55 +1000

    adding number styling to cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './Card.css';
 
+// const Card = ({ rank, suit, value }) => {
+//   return(
+//     <div className="card">
+//       <div className="card-rank">{rank}</div>
+//       <div className={`card-suit ${suit}`}>{suit}</div>
+//       <div className="card-value">{value}</div>
+//     </div>
+//   )
+
+
+// }
+
 const Card = ({ rank, suit, value }) => {
   return(
     <div className="card">
-      <div className="card-rank">{rank}</div>
-      <div className={`card-suit ${suit}`}>{suit}</div>
-      <div className="card-value">{value}</div>
+      <h1 className='rank top'>
+        {rank}
+      </h1>
+      <h1 className='rank bottom'>
+        {rank}
+      </h1>
     </div>
   )
 
 
 }
+
 
 export default Card;
