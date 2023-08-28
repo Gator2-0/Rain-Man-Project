84f766eeb6a0b8d447e60063388198e285e62ff8
Jeremy <jejedech.63@gmail.com>
2023-08-28 19:16:21 +1000

    simple code clean up

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   BsFillSuitHeartFill,
   BsFillSuitDiamondFill,
@@ -28,6 +28,7 @@ function SuitIcon({ suit, style }) {
 
 const Card = ({ rank, suit, value, isFaceUp }) => {
 
+  const myRef = useRef(null);
   const [suitColor, setSuitColor] = useState(""); // Define suitColor state 
   useEffect(() => {
     suit === "hearts" || suit === "diamonds"
@@ -35,41 +36,68 @@ const Card = ({ rank, suit, value, isFaceUp }) => {
       : setSuitColor("black");
   }, [suit]); 
 
-  if(!isFaceUp){
-    return (
-      <div className='card'>
-        <img
-            src={CardBack} // Specify the image path
-            alt="Card back image"
-            style={{
-              width: "100%", // Adjust dimensions as needed
-              height: "100%",
-            }}
-          />
-      </div>
-    )
+  function flipCard() {
+    if (myRef.current.style.transform) {
+      myRef.current.style.transform = "";
+    } else {
+      myRef.current.style.transform = "rotateY(180deg)";
+    }
   }
+
   return(
-   
-        <div className="card">
-          <h1 className='rank top'>
-            {rank}
-          </h1>
-          <SuitIcon
-            className="suitIcon"
-            suit={suit}
-            style={{
-              fontSize: "25px",
-              position: "absolute",
-              margin: "0px",
-              top: "60px",
-              left: "38px",
-              color: suitColor,
-            }}
-          />  
-          <h1 className='rank bottom'>
-            {rank}
-          </h1>
+    
+        <div className='card' 
+          ref={myRef}
+          onClick={flipCard}>
+
+          <div
+              className="card-inner"
+              style={{
+                width: '100%',
+                height: '100%',
+                transformStyle: 'preserve-3d',
+                transition: 'transform 0.5s', // Add transition for smoother animation
+                transform: 'rotateY(180deg)',
+              }}
+            >
+              <div style={{ backfaceVisibility: "hidden" }}>
+                <h1 className='rank top'>
+                  {rank}
+                </h1>
+                <SuitIcon
+                  className="suitIcon"
+                  suit={suit}
+                  style={{
+                    fontSize: "25px",
+                    position: "absolute",
+                    margin: "0px",
+                    top: "60px",
+                    left: "30px",
+                    color: suitColor,
+                  }}
+                />  
+                <h1 className='rank bottom'>
+                  {rank}
+                </h1>
+              </div>
+
+              <div
+                style={{
+                  backfaceVisibility: "hidden",
+                  transform: "rotateY(180deg)",
+                }}
+              >
+                <img
+                  src={CardBack} // Specify the image path
+                  alt="Card back"
+                  style={{
+                    width: "100%", // Adjust dimensions as needed
+                    height: "100%",
+                  }}
+                />
+              </div>
+            </div>
+
         </div>
   )
 }
